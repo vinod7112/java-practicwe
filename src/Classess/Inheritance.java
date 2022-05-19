14f5e2fc93b313ffe4f230c00098f2b81e002b95
vinod7112 <103653456+vinod7112@users.noreply.github.com>
2022-05-19 18:40:58 +0530

    Add files via upload

---
+++ src/Classess/Inheritance.java
@@ -0,0 +1,15 @@
+package OopsPractice;
+
+ class Inheritance {
+	String str="world";
+ }
+	  class automation extends Inheritance {
+		int salary = 3000000;
+		public static void main(String[] args) {
+			automation at = new automation();
+			System.out.println("ctc are:"+at.salary+at.str);
+			
+		
+	}
+
+}
